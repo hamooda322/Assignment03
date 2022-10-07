feaba99e4449adeb13c2eeeeac7383240e51e5a1
hamooda322 <113495325+hamooda322@users.noreply.github.com>
2022-10-07 00:05:45 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        char[] name = {'s', 'a', 'b', 'a'};
+
+        for (int i = 0; i < name.length; i++){
+            int numericValueOfMyChar = (int)name[i];
+            int upperCaseNumericValue = numericValueOfMyChar - 32;
+
+        }
+    }
+}
